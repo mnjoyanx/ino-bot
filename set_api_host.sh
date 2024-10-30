6c252a1eea654ebbe5c8269511ff08f70db63d1a
Andranik Vardanyan <a.vardanyan@inorain.com>
2024-10-30 09:58:02 +0000

    Update set_api_host.sh

--- set_api_host.sh
+++ set_api_host.sh
@@ -1,6 +1,7 @@
 FILE_PATH=$(find /usr/share/nginx/html -type f -name "*.js")
 
+DEFAULT_API_HOST="API_HOST"
 
 for file in $FILE_PATH; do
-    sed -i "s|{{API_HOST}}|$API_HOST|g" "$file"
+    sed -i "s|$DEFAULT_API_HOST|$API_HOST|g" "$file"
 done
