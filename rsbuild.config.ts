c50e200994a72ee75ad0b0be707af03c401d40b4
mnjoyanx <tigranmnjoyan@gmail.com>
2024-12-16 11:12:41 +0400

    dist

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
       html: "index.html",
     },
     distPath: {
+      root: "build",
       js: ".",
       css: ".",
       html: ".",
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,6 @@
 import { Provider } from "react-redux";
 
 import { createRoot } from "react-dom/client";
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 
 import App from "./App.jsx";
 import store from "./app/store.js";
