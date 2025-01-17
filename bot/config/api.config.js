c852c6d9038bb651aa3c1cf31912db5248a70ed0
mnjoyanx <tigranmnjoyan@gmail.com>
2025-01-17 13:54:09 +0400

    fix

--- bot/config/api.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  JWT_SECRET:
-    process.env.JWT_SECRET ||
-    "v2098j2#(K@v08vh293v8hp97GP(7G8p7gpqa9wubh2##LKJklasdn23n",
-  API_PORT: process.env.API_PORT || 3000,
-  MONGODB_URI: process.env.MONGODB_URI,
-  ALLOWED_ORIGINS: ["http://localhost:5173"],
-};
